6d8d8fef6fade695529dbb0a1bd5a616a60f29fa
dkatina <108095842+dkatina@users.noreply.github.com>
2025-02-13 11:45:59 -0500

    first commit - arithmetic

---
+++ src/Arithmetic.java
@@ -0,0 +1,12 @@
+public class Arithmetic {
+
+
+
+    public static void main(String[] args) {
+        //PEMDAS
+        int num = 2 + 6 * 3 - 5 *4;
+        System.out.println(num);
+
+        //numeric promotion
+    }
+}
